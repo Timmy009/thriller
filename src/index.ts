8020cf4189a62b193370b290994c495627ab4947
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2024-02-25 15:47:12 +0100

    realf

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,7 @@ import session from 'express-session';
   const port = config.get<number>("port");
  const MongoStore = connectMongo(session);
 app.use(cookieParser());
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use(
